42f42f1e947d0e346d2d70dd3ad07d13cf8e670b
Pratik <71585947+Pratik543@users.noreply.github.com>
2026-08-12 22:39:59 +0530

    chore(terraform): update S3 state key to terraform/.
    - Align remote state path with the terraform-iac/ to terraform/
      directory migration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "task-master-pro-terraform-state"
-    key     = "terraform-iac/terraform.tfstate"
+    key     = "terraform/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
   }
